48a3e806f563bd912d669b49fb17f9438089e734
Ricardo Novais <ricardonovais1@gmail.com>
2022-07-13 15:49:59 -0400

    Randomico + botão mostrando resultado no console

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
-function computerPlay(computerChoice) {
+let computerSelection = computerPlay();
+let playerSelection = window.prompt('Pedra, papel ou tesoura?');
+const displayResult_button = document.querySelector('.display-result');
+
+
+displayResult_button.addEventListener('click', function() {
+    singleRound(playerSelection.toLocaleLowerCase(), computerSelection);
+});
+
+
+function computerPlay() {
     let randomOption = Math.floor(Math.random() * 3)
-    if (randomOption === 0)
-    {
-        console.log('computerChoice é Pedra');
-    } 
-    else if (randomOption === 1)
-    {
-        console.log('computerChoice é Papel');
-    }   else 
-    {
-        console.log('computerChoice é tesoura');
+    switch (randomOption) {
+    case 0:
+        return 'pedra';
+        break;
+    case 1:
+        return 'papel'
+        break;
+    case 2:
+        return 'tesoura'
+        break;
     }
+}
+
+console.log(`O computador escolheu ${computerPlay()}`)
+console.log(`Você escolheu ${playerSelection}`)
+
+
+function singleRound(playerSelection, computerSelection) {
+/*     let optionsMatch = playerSelection + computerSelection;
+        
+ */    
+
+    switch(playerSelection + computerSelection) {
+        case 'pedratesoura':
+        case 'papelpedra':
+        case 'tesourapapel':
+            console.log('Você venceu')
+            break
+        case 'pedrapapel':
+        case 'papeltesoura':
+        case 'tesourapedra':
+            console.log('Computador Venceu')
+            break
+        case 'pedrapedra':
+        case 'papelpapel':
+        case 'tesouratesoura':
+            console.log('Empate')
+            break
     }
-computerPlay()
+}
+
+ 
+    
+
+   
+
+
 
