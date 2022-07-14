bc0bce7aab02e535156322c77851a2cc688c37af
Ricardo Novais <ricardonovais1@gmail.com>
2022-07-14 15:28:46 -0400

    Código mais limpo, com comentários e a função de dizer o nome do vencedor ao final

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-let playerSelection = '' 
+let playerSelection = '';
 const displayResult_button = document.querySelector('.display-result');
 let computerScore = 0;
 let playerScore = 0;
 
-/* displayResult_button.addEventListener('click', function() {
-    playRound(playerSelection.toLocaleLowerCase(), computerSelection);
-}); */
-
-
-
-document.body.addEventListener('keypress', (e) => {
-    if (e.key == 'Enter') playRound(playerSelection.toLocaleLowerCase(), computerSelection);
-    
-});
-
+//Selecionar a opção do comuputador de forma randômica:
 
 function computerPlay() {
     let randomOption = Math.floor(Math.random() * 3)
@@ -27,63 +17,43 @@ function computerPlay() {
     case 2:
         return 'tesoura'
         break;
-    }
-    
-
+    }   
 }
 
-/* console.log(`O computador escolheu ${computerPlay()}`)
-console.log(`Você escolheu ${playerSelection}`) */
-
+//Jogar uma única partida de Pedra papel e tesoura
+//Aumentar em 1 ponto para quem ganhar
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'pedra' && computerSelection === 'pedra') {
-        return 'Empate! Você e o computador escolheram PEDRA!'
+        return 'Empate! Você e o computador escolheram PEDRA!';
     } else if (playerSelection === 'papel' && computerSelection === 'papel') {
-        return 'Empate! Você e o computador escolheram PAPEL!'
+        return 'Empate! Você e o computador escolheram PAPEL!';
     } else if (playerSelection === 'tesoura' && computerSelection === 'tesoura') {
-        return 'Empate! Você e o computador escolheram TESOURA!' 
+        return 'Empate! Você e o computador escolheram TESOURA!' ;
     } else if (playerSelection === 'pedra' && computerSelection === 'tesoura') {
         playerScore++
-        return 'Você venceu! Pedra vence tesoura!' 
+        return 'Você venceu! Pedra vence tesoura!'; 
     } else if (playerSelection === 'pedra' && computerSelection === 'papel') {
         computerScore++ 
-        return 'Você perdeu! Pedra perde do papel...'
+        return 'Você perdeu! Pedra perde do papel...';
     } else if (playerSelection === 'papel' && computerSelection === 'pedra') {
         playerScore++
-        return 'Você venceu! Papel vence pedra!' 
+        return 'Você venceu! Papel vence pedra!'; 
     } else if (playerSelection === 'papel' && computerSelection === 'tesoura') {
         computerScore++ 
-        return 'Você perdeu! Papel perde da tesoura...' 
+        return 'Você perdeu! Papel perde da tesoura...';
     } else if (playerSelection === 'tesoura' && computerSelection === 'papel') {
         playerScore++
-        return 'Você venceu! Tesoura vence papel!' 
+        return 'Você venceu! Tesoura vence papel!'; 
     } else if (playerSelection === 'tesoura' && computerSelection === 'pedra') {
         computerScore++ 
-        return 'Você perdeu! Tesoura perde da pedra...' 
+        return 'Você perdeu! Tesoura perde da pedra...'; 
 
     } 
     playerRound(playerSelection, computerSelection)
-   /*  switch(playerSelection + computerSelection) {
-        case 'pedratesoura':
-        case 'papelpedra':
-        case 'tesourapapel':
-            console.log('Você venceu')
-            playerScore++
-            break
-        case 'pedrapapel':
-        case 'papeltesoura':
-        case 'tesourapedra':
-            console.log('Computador Venceu')
-            computerScore++
-            break
-        case 'pedrapedra':
-        case 'papelpapel':
-        case 'tesouratesoura':
-            console.log('Empate')
-            break
-    } */
-}
+  }
+
+  //Fazer com que sejam jogadas 5 partidas
 
 const game = () => {
     for(let i = 0; i < 5; i++) {
@@ -91,9 +61,16 @@ const game = () => {
         let computerSelection = computerPlay();
         console.log(playRound(playerSelection, computerSelection));
         console.log(`Pontos robô: ${computerScore}`);
-        console.log(`Pontos você: ${playerScore}`)
-        
+        console.log(`Pontos você: ${playerScore}`);
     }
+    if (computerScore > playerScore) {
+        console.log(`Você perdeu para o computador por ${computerScore} a ${playerScore}`);
+    } else if (computerScore < playerScore) {
+        console.log(`Parabéns! Você ganhou do computador por ${playerScore} a ${computerScore}`)
+    } else {
+        console.log(`Você e o computador empataram em ${computerScore} a ${playerScore}`)
+    }
+
 }
 
 game()
