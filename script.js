d73a05b6fcd7f9c750ccfc9dbff8988281a31fcd
Ricardo Novais <ricardonovais1@gmail.com>
2022-07-14 14:06:20 -0400

    Consegui finalmente realizar o for loop e passar de fase

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let computerSelection = computerPlay();
-let playerSelection = window.prompt('Pedra, papel ou tesoura?');
+let playerSelection = '' 
 const displayResult_button = document.querySelector('.display-result');
+let computerScore = 0;
+let playerScore = 0;
+
+/* displayResult_button.addEventListener('click', function() {
+    playRound(playerSelection.toLocaleLowerCase(), computerSelection);
+}); */
 
 
-displayResult_button.addEventListener('click', function() {
-    singleRound(playerSelection.toLocaleLowerCase(), computerSelection);
+
+document.body.addEventListener('keypress', (e) => {
+    if (e.key == 'Enter') playRound(playerSelection.toLocaleLowerCase(), computerSelection);
+    
 });
 
 
@@ -21,37 +28,75 @@ function computerPlay() {
         return 'tesoura'
         break;
     }
+    
+
 }
 
-console.log(`O computador escolheu ${computerPlay()}`)
-console.log(`Você escolheu ${playerSelection}`)
+/* console.log(`O computador escolheu ${computerPlay()}`)
+console.log(`Você escolheu ${playerSelection}`) */
 
 
-function singleRound(playerSelection, computerSelection) {
-/*     let optionsMatch = playerSelection + computerSelection;
-        
- */    
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'pedra' && computerSelection === 'pedra') {
+        return 'Empate! Você e o computador escolheram PEDRA!'
+    } else if (playerSelection === 'papel' && computerSelection === 'papel') {
+        return 'Empate! Você e o computador escolheram PAPEL!'
+    } else if (playerSelection === 'tesoura' && computerSelection === 'tesoura') {
+        return 'Empate! Você e o computador escolheram TESOURA!' 
+    } else if (playerSelection === 'pedra' && computerSelection === 'tesoura') {
+        playerScore++
+        return 'Você venceu! Pedra vence tesoura!' 
+    } else if (playerSelection === 'pedra' && computerSelection === 'papel') {
+        computerScore++ 
+        return 'Você perdeu! Pedra perde do papel...'
+    } else if (playerSelection === 'papel' && computerSelection === 'pedra') {
+        playerScore++
+        return 'Você venceu! Papel vence pedra!' 
+    } else if (playerSelection === 'papel' && computerSelection === 'tesoura') {
+        computerScore++ 
+        return 'Você perdeu! Papel perde da tesoura...' 
+    } else if (playerSelection === 'tesoura' && computerSelection === 'papel') {
+        playerScore++
+        return 'Você venceu! Tesoura vence papel!' 
+    } else if (playerSelection === 'tesoura' && computerSelection === 'pedra') {
+        computerScore++ 
+        return 'Você perdeu! Tesoura perde da pedra...' 
 
-    switch(playerSelection + computerSelection) {
+    } 
+    playerRound(playerSelection, computerSelection)
+   /*  switch(playerSelection + computerSelection) {
         case 'pedratesoura':
         case 'papelpedra':
         case 'tesourapapel':
             console.log('Você venceu')
+            playerScore++
             break
         case 'pedrapapel':
         case 'papeltesoura':
         case 'tesourapedra':
             console.log('Computador Venceu')
+            computerScore++
             break
         case 'pedrapedra':
         case 'papelpapel':
         case 'tesouratesoura':
             console.log('Empate')
             break
+    } */
+}
+
+const game = () => {
+    for(let i = 0; i < 5; i++) {
+        let playerSelection = window.prompt('Pedra, papel ou tesoura?').toLocaleLowerCase();
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Pontos robô: ${computerScore}`);
+        console.log(`Pontos você: ${playerScore}`)
+        
     }
 }
 
- 
+game()
     
 
    
