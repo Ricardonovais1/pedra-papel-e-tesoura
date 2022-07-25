2d1f88c3ffacef612f21c54008bc7896cbabc994
Ricardo Novais <ricardonovais1@gmail.com>
2022-07-25 07:17:59 -0400

    limpeza e organização do código, além de ajustes como animações

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
-
 //Cash the DOM:
-const title_h1 = document.querySelector('title');
+const title_h1 = document.querySelector('title-h1');
 const botaoIniciar_div = document.querySelector('botao-iniciar');
 const choice_div = document.querySelector('choice');
 const pedra = document.getElementById('pedra');
@@ -11,6 +9,8 @@ const playerScore_span = document.querySelector('.player-score');
 const computerScore_span = document.querySelector('.computer-score');
 const roundResult_div = document.querySelector('.round-result');
 const finalResult_div = document.querySelector('.final-result');
+const scoreBoard_div = document.querySelector('.score-board');
+
 
 var playerScore = 0;
 var computerScore = 0;
@@ -19,6 +19,7 @@ let playerSelection = '';
 const displayResult_button = document.querySelector('.display-result');
 
 
+// Início do jogo 
 const main = () => {
     pedra.addEventListener('click', () => {
         playRound('pedra');
@@ -28,17 +29,32 @@ const main = () => {
     papel.addEventListener('click', () => {
         playRound('papel');
         game();
-
     });
 
     tesoura.addEventListener('click', () => {
         playRound('tesoura');
         game();
-
     });
 }; 
 
 main();
+
+//Selecionar a opção do computador de forma randômica:
+
+function computerPlay() {
+    let randomOption = Math.floor(Math.random() * 3);
+    switch (randomOption) {
+    case 0:
+        return 'pedra';
+        break;
+    case 1:
+        return 'papel'
+        break;
+    case 2:
+        return 'tesoura'
+        break;
+    };   
+};
 
 
 const win = (playerSelection, computerSelection) => {
@@ -59,68 +75,7 @@ const draw = (playerSelection, computerSelection) => {
     roundResult_div.textContent = `Empate, ${playerSelection} vs. ${computerSelection}`;
 };
 
-
-//Selecionar a opção do comuputador de forma randômica:
-
-function computerPlay() {
-    let randomOption = Math.floor(Math.random() * 3);
-    switch (randomOption) {
-    case 0:
-        return 'pedra';
-        break;
-    case 1:
-        return 'papel'
-        break;
-    case 2:
-        return 'tesoura'
-        break;
-    };   
-};
-
-//Jogar uma única partida de Pedra papel e tesoura
-//Aumentar em 1 ponto para quem ganhar
-
-/* function playRound(playerSelection, computerSelection) {
-
-    userScore = 0;
-    computerScore = 0;
-
-    if (playerSelection === 'pedra' && computerSelection === 'pedra') {
-        return 'Empate! Você e o computador escolheram PEDRA!';
-        draw();
-    } else if (playerSelection === 'papel' && computerSelection === 'papel') {
-        return 'Empate! Você e o computador escolheram PAPEL!';
-        draw();
-    } else if (playerSelection === 'tesoura' && computerSelection === 'tesoura') {
-        return 'Empate! Você e o computador escolheram TESOURA!' ;
-        draw();
-    } else if (playerSelection === 'pedra' && computerSelection === 'tesoura') {
-        playerScore++
-        return 'Você venceu! Pedra vence tesoura!'; 
-        win();
-    } else if (playerSelection === 'pedra' && computerSelection === 'papel') {
-        computerScore++ 
-        return 'Você perdeu! Pedra perde do papel...';
-        win();
-    } else if (playerSelection === 'papel' && computerSelection === 'pedra') {
-        playerScore++
-        return 'Você venceu! Papel vence pedra!'; 
-        win();
-    } else if (playerSelection === 'papel' && computerSelection === 'tesoura') {
-        computerScore++ 
-        return 'Você perdeu! Papel perde da tesoura...';
-    } else if (playerSelection === 'tesoura' && computerSelection === 'papel') {
-        playerScore++
-        return 'Você venceu! Tesoura vence papel!'; 
-    } else if (playerSelection === 'tesoura' && computerSelection === 'pedra') {
-        computerScore++ 
-        return 'Você perdeu! Tesoura perde da pedra...'; 
-
-    } 
-    playRound(playerSelection, computerSelection);
-  }; */
-
-  //Fazer com que sejam jogadas 5 partidas
+  //Fazer com que seja jogada 1 única partida:
 
   const playRound = (playerSelection) => {
 
@@ -147,14 +102,18 @@ function computerPlay() {
            
     };
     endGame(playerScore, computerScore);
-
   };
+
+
+
+  // Permitir que o jogador que alcançar 5 pontos vença o jogo:
+
+
 
   const endGame = (playerScore, computerScore) => {
     if (playerScore < 5 && computerScore < 5) 
     {
         playRound(playerSelection);
-
     } 
     else if (playerScore === 5 && computerScore < 5) 
     {
@@ -163,8 +122,9 @@ function computerPlay() {
         pedra.classList.add('hide');
         papel.classList.add('hide');
         tesoura.classList.add('hide');
-
-
+        const scoreBoard_div = document.querySelector('score-board');
+        scoreBoard_div.classList.add('pulse');
+        const titleWhenResult = document.getElementById('title').textContent = `RESULTADO FINAL:`;  
     }
     else if (computerScore === 5 && playerScore < 5) 
     {
@@ -173,34 +133,12 @@ function computerPlay() {
         pedra.classList.add('hide');
         papel.classList.add('hide');
         tesoura.classList.add('hide');
-
-
+        scoreBoard_div.classList.add('pulse');
+        const titleWhenResult = document.getElementById('title').textContent = `RESULTADO FINAL:`;  
     }
   };
-   /*  const game = () => {
-    for(let i = 0; i < 5; i++) {
 
-
-        
-    };
-    if (computerScore > playerScore) {
-        finalResult_div.textContent = `R. FINAL: Você perdeu para o computador por ${computerScore} a ${playerScore}`;
-    } else if (computerScore < playerScore) {
-        finalResult_div.textContent = `R. FINAL: Parabéns! Você ganhou do computador por ${playerScore} a ${computerScore}`;
-    } else {
-        finalResult_div.textContent = `R. FINAL: Você e o computador empataram em ${computerScore} a ${playerScore}`;
-    };
-
-};
-
-game()   */
-
-//---------------------------------------------------------------
-    
-/* console.log(playRound(playerSelection, computerSelection));
-        console.log(`Pontos robô: ${computerScore}`);
-        console.log(`Pontos você: ${playerScore}`); */
-   
-
-
+  function titleResult() { 
+     
+  };
 
