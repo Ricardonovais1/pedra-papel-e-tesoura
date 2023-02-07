5fbc54d196932aa1d6981b7ca1c68ad7434d000c
Ricardo Miranda de Novais <ricardonovais1@gmail.com>
2023-02-06 21:32:29 -0300

    Botão de reiniciar + bug fixes

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const computerScore_span = document.querySelector('.computer-score');
 const roundResult_div = document.querySelector('.round-result');
 const finalResult_div = document.querySelector('.final-result');
 const scoreBoard_div = document.querySelector('.score-board');
+const result = document.querySelector('.result');
 
 
 var playerScore = 0;
@@ -18,22 +19,18 @@ var computerScore = 0;
 let playerSelection = '';
 const displayResult_button = document.querySelector('.display-result');
 
-
 // Início do jogo
 const main = () => {
     pedra.addEventListener('click', () => {
         playRound('pedra');
-        game();
     });
 
     papel.addEventListener('click', () => {
         playRound('papel');
-        game();
     });
 
     tesoura.addEventListener('click', () => {
         playRound('tesoura');
-        game();
     });
 };
 
@@ -101,30 +98,30 @@ const draw = (playerSelection, computerSelection) => {
             break;
 
     };
-    endGame(playerScore, computerScore);
+
+    if (playerScore === 5 || computerScore === 5) endGame(playerScore, computerScore);
+
   };
 
 
 
   // Permitir que o jogador que alcançar 5 pontos vença o jogo:
-
-
-
   const endGame = (playerScore, computerScore) => {
-    if (playerScore < 5 && computerScore < 5)
-    {
-        playRound(playerSelection);
-    }
-    else if (playerScore === 5 && computerScore < 5)
+
+    if (playerScore === 5 && computerScore < 5)
     {
         finalResult_div.textContent = `Parabéns! Você ganhou do computador por ${playerScore} a ${computerScore}`;
         roundResult_div.classList.add('hide');
         pedra.classList.add('hide');
         papel.classList.add('hide');
         tesoura.classList.add('hide');
-        const scoreBoard_div = document.querySelector('score-board');
         scoreBoard_div.classList.add('pulse');
         const titleWhenResult = document.getElementById('title').textContent = `RESULTADO FINAL:`;
+
+        setTimeout(() => {
+            replayButton()
+        }, 1000);
+
     }
     else if (computerScore === 5 && playerScore < 5)
     {
@@ -135,5 +132,25 @@ const draw = (playerSelection, computerSelection) => {
         tesoura.classList.add('hide');
         scoreBoard_div.classList.add('pulse');
         const titleWhenResult = document.getElementById('title').textContent = `RESULTADO FINAL:`;
+
+        setTimeout(() => {
+            replayButton()
+        }, 1000);
     }
   };
+
+  const replayButton = () => {
+
+    let replayBtn = document.createElement('button');
+    replayBtn.className = 'replay';
+    replayBtn.textContent = 'Jogar de novo';
+
+    result.appendChild(replayBtn);
+
+    replayBtn.classList.add('show');
+    replayBtn.classList.add('pulse');
+
+    replayBtn.addEventListener('click', () => { location.reload() });
+
+
+  }
